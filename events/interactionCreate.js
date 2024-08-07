3444a55c62c262357d289ffe390b8ad711187787
nahueluna <nahuelnicolasluna@hotmail.com>
2024-08-07 20:35:07 -0300

    Eventos ready e interactionCreate modularizados

---
+++ events/interactionCreate.js
@@ -0,0 +1,21 @@
+export default {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if (!interaction.isCommand()) return;
+    
+        const command = interaction.client.commands.get(interaction.commandName);
+    
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(`Error executing ${interaction.commandName}`);
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    },
+};
